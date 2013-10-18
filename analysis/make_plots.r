a8e7548ea083a239f5cbd475a8cd4308b85ad9b3
Morten Olsen Lysgaard <morten@lysgaard.no>
2013-10-18 16:51:13 +1000

    updated report, updated make_plots.r

--- analysis/make_plots.r
+++ analysis/make_plots.r
@@ -2,14 +2,17 @@
 
 library(gdata)
 
+pdf('serial_walltime.pdf')
 serData = read.csv('ser-1k-10k-20runs')
 boxplot(time~n,data=serData, log="y", main="Serial wall time as a function of n",
-	   xlab="n", ylab="wall time")
+	   xlab="n", ylab="wall time(s)")
 
+pdf('parallel_walltime1.pdf')
 parData = read.csv('par-1k-10k-10runs')
 boxplot(time.after.gather~n,data=parData, log="y", main="Parallel wall time as a function of n",
-	   xlab="n", ylab="wall time")
+	   xlab="n", ylab="wall time(s)")
 
+pdf('problem_size_speedup.pdf')
 library(plyr)
 serMean = ddply(serData,~n,summarize,meanTime=mean(time))
 parMean = ddply(parData,~n,summarize,meanTime=mean(time.after.gather))
@@ -17,11 +20,13 @@ plot(serMean$n, serMean$meanTime/parMean$meanTime, xlab="n", ylab="speedup", mai
 
 # changing node count stuff
 
+pdf('mpi_nodes_walltime.pdf')
 parNData = read.csv('par-1n-36n-2mpi-10k')
-boxplot(time.after.gather~mpi.nodes,data=parNData, log="y", main="Parallel wall time as a function of mpi nodes",
-	   xlab="mpi nodes", ylab="wall time")
+boxplot(time.after.gather~mpi.nodes,data=parNData, log="y", main="Parallel wall time as a function of mpi processes",
+	   xlab="mpi processes", ylab="wall time(s)")
 
+pdf('mpi_nodes_speedup.pdf')
 parNMean = ddply(parNData,~mpi.nodes,summarize,meanTime=mean(time.after.gather))
 parNMean$mpi.nodes
 rep(serMean$meanTime[10],times=6)/parNMean$meanTime
-plot(parNMean$mpi.nodes, rep(serMean$meanTime[10],times=6)/parNMean$meanTime, xlab="MPI nodes", ylab="speedup", main="Speedup as a function of MPI nodes")
+plot(parNMean$mpi.nodes, rep(serMean$meanTime[10],times=6)/parNMean$meanTime, xlab="MPI processes", ylab="speedup", main="Speedup as a function of MPI processes")
